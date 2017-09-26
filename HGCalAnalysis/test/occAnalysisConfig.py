b9fbef226a5df26e39c46252fe0597e647ec5678
myname <mymail@cern.ch>
2017-09-26 17:47:08 +0200

    adding pu200

--- HGCalAnalysis/test/occAnalysisConfig.py
+++ HGCalAnalysis/test/occAnalysisConfig.py
@@ -4,7 +4,8 @@ from Configuration.StandardSequences.Eras import eras
 process = cms.Process("HGCOcc")
 process.load('Configuration.StandardSequences.Services_cff')
 process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
-process.load('Configuration.Geometry.GeometryExtended2023D17Reco_cff')
+#process.load('Configuration.Geometry.GeometryExtended2023D17Reco_cff')
+process.load('Configuration.Geometry.GeometryExtended2023D13Reco_cff')
 process.load('Configuration.StandardSequences.MagneticField_cff')
 process.load('Configuration.EventContent.EventContent_cff')
 process.load("FWCore.MessageService.MessageLogger_cfi")
@@ -20,7 +21,8 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
     fileNames = cms.untracked.vstring(
-        '/store/relval/CMSSW_9_1_0_pre3/RelValDoubleMuPt15Eta17_27/GEN-SIM-RECO/91X_upgrade2023_realistic_v1_D13noSmearPU140-v1/10000/1418DDB5-FE35-E711-B4B8-0025905A60B6.root'
+        '/store/relval/CMSSW_9_1_0_pre3/RelValDoubleMuPt15Eta17_27/GEN-SIM-RECO/91X_upgrade2023_realistic_v1_D13noSmearPU200-v1/10000/1CFC2232-0036-E711-B417-0025905B85DA.root'
+        #'/store/relval/CMSSW_9_1_0_pre3/RelValDoubleMuPt15Eta17_27/GEN-SIM-RECO/91X_upgrade2023_realistic_v1_D13noSmearPU140-v1/10000/1418DDB5-FE35-E711-B4B8-0025905A60B6.root'
         #'/store/relval/CMSSW_9_1_0_pre3/RelValDoubleMuPt15Eta17_27/GEN-SIM-RECO/91X_upgrade2023_realistic_v1_D13noSmear-v1/10000/1E2EA7BF-B035-E711-BD66-0025905A60B8.root'
     ),
     duplicateCheckMode = cms.untracked.string("noDuplicateCheck")
@@ -29,7 +31,8 @@ process.source = cms.Source("PoolSource",
 process.ana = cms.EDAnalyzer("HGCOccupancyAnalyzer")
                              
 process.TFileService = cms.Service("TFileService",
-                                   fileName = cms.string("occ_analysis_pu140.root")
+                                   fileName = cms.string("occ_analysis_pu200.root")
+                                   #fileName = cms.string("occ_analysis_pu140.root")
                                    #fileName = cms.string("occ_analysis_pu0.root")                                                      
                                    )
 process.p = cms.Path(process.ana)
